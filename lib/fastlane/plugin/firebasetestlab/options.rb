1b2ffbf30fd2b4c8aec643732d3403a627773f3e
powerivq <poweriv.q@gmail.com>
2018-08-01 15:28:00 -0700

    Update comments

--- lib/fastlane/plugin/firebasetestlab/options.rb
+++ lib/fastlane/plugin/firebasetestlab/options.rb
@@ -1,7 +1,7 @@
 require 'fastlane_core/configuration/config_item'
 
 module Fastlane
-  module Ftl
+  module Firebasetestlab
     class Optinos
       def self.available_options
         [
@@ -10,7 +10,6 @@ module Fastlane
                                        optional: false),
           FastlaneCore::ConfigItem.new(key: :app_path,
                                        description: "Path to the app, either on the filesystem or GCS address (gs://)",
-                                       optional: false,
                                        verify_block: proc do |value|
                                          unless value.to_s.start_with?("gs://")
                                            v = File.expand_path(value.to_s)
@@ -19,37 +18,36 @@ module Fastlane
                                        end),
           FastlaneCore::ConfigItem.new(key: :devices,
                                        description: "Devices to test the app on",
-                                       optional: false,
                                        type: Array,
                                        verify_block: proc do |value|
                                          value.each do |current|
                                            UI.user_error!("Each device must be represented by a Hash object, #{current.class} found") if current.class != Hash
                                            check_has_property(current, :iosModelId)
                                            check_has_property(current, :iosVersionId)
-                                           check_has_property(current, :locale)
-                                           check_has_property(current, :orientation)
+                                           set_default_property(current, :locale, "en_US")
+                                           set_default_property(current, :orientation, "portrait")
                                          end
                                        end),
           FastlaneCore::ConfigItem.new(key: :async,
                                        description: "Do not wait for test results",
                                        default_value: false,
-                                       verify_block: proc do |value|
-                                         UI.user_error!("async must be either true or false") if value != true && value != false
-                                       end),
+                                       type: Fastlane::Boolean),
           FastlaneCore::ConfigItem.new(key: :timeout_sec,
                                        description: "After how long, in seconds, should tests be terminated",
-                                       default_value: 900,
+                                       default_value: 180,
+                                       optional: true,
                                        verify_block: proc do |value|
                                          UI.user_error!("Timeout must be a positive number") if value <= 0
                                        end),
           FastlaneCore::ConfigItem.new(key: :result_storage,
                                        description: "GCS path to store test results",
                                        default_value: nil,
+                                       optional: true,
                                        verify_block: proc do |value|
                                          UI.user_error!("Invalid GCS path: '#{value}'") unless value.match(/^gs:\/\/.*\//)
                                        end),
-          FastlaneCore::ConfigItem.new(key: :oauth_key_file_override,
-                                       description: "Use this key instead of application default credential",
+          FastlaneCore::ConfigItem.new(key: :oauth_key_file,
+                                       description: "Use the given Google cloud service key file. If not set, application default credential will be used (see https://cloud.google.com/docs/authentication/production)",
                                        default_value: nil,
                                        optional: true,
                                        verify_block: proc do |value|
@@ -61,6 +59,12 @@ module Fastlane
 
       def self.check_has_property(hash_obj, property)
         UI.user_error!("Each device must have #{property} property") unless hash_obj.has_key?(property)
+      end
+
+      def self.set_default_property(hash_obj, property, default)
+        unless hash_obj.has_key?(property)
+          hash_obj[property] = default
+        end
       end
     end
   end
