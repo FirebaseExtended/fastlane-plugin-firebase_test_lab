0204c91e60bfec4d377788663c0c5cf7b92b7747
Shihua Zheng <powerivq@users.noreply.github.com>
2018-08-06 14:44:55 -0700

    Add Google credential helper (#5)
    * Google credential helper
    
    * OOOOOPS
    
    * typo
    
    * Rename a few things
    
    * default
    
    * maybe like this more
    
    * Update logic

---
+++ lib/fastlane/plugin/firebase_test_lab/helper/credential.rb
@@ -0,0 +1,19 @@
+require 'googleauth'
+
+module Fastlane
+  module FirebaseTestLab
+    class Credential
+      def initialize(key_file_path: nil)
+        @key_file_path = key_file_path
+      end
+
+      def self.get_google_credential(scopes)
+        return Google::Auth.get_application_default(scopes) unless @key_file_path
+
+        File.open(@key_file_path, "r") do |file|
+          return Google::Auth::ServiceAccountCredentials.read_json_key(file)
+        end
+      end
+    end
+  end
+end
