170edbca9e3ae90cda50b6ffef111587f62a2b17
powerivq <poweriv.q@gmail.com>
2018-08-08 17:44:22 -0700

    Fine tune print messages

--- lib/fastlane/plugin/firebase_test_lab/actions/firebase_test_lab_ios_xctest.rb
+++ lib/fastlane/plugin/firebase_test_lab/actions/firebase_test_lab_ios_xctest.rb
@@ -13,8 +13,20 @@ module Fastlane
       DEFAULT_APP_BUNDLE_NAME = "bundle"
       PULL_RESULT_INTERVAL = 5
 
+      ERROR_CODE_TO_MESSAGE = {
+        ERROR: "The execution or matrix has stopped because it encountered an infrastructure failure.",
+        UNSUPPORTED_ENVIRONMENT: "The execution was not run because it corresponds to a unsupported environment.",
+        INCOMPATIBLE_ENVIRONMENT: "The execution was not run because the provided inputs are incompatible with the " \
+                                  "requested environment",
+        INCOMPATIBLE_ARCHITECTURE: "The execution was not run because the provided inputs are incompatible with the " \
+                                   "requested architecture.",
+        CANCELLED: "The user cancelled the execution.",
+        INVALID: "The execution or matrix was not run because the provided inputs are not valid.",
+      }
+
       private_constant :DEFAULT_APP_BUNDLE_NAME
       private_constant :PULL_RESULT_INTERVAL
+      private_constant :ERROR_CODE_TO_MESSAGE
 
       def self.run(params)
         gcp_project = params[:gcp_project]
@@ -27,7 +39,7 @@ module Fastlane
         if params[:app_path].to_s.start_with?("gs://")
           app_gcs_link = params[:app_path]
         else
-          upload_spinner = TTY::Spinner.new("[:spinner] Uploading the app to GCS", format: :dots)
+          upload_spinner = TTY::Spinner.new("[:spinner] Uploading the app to GCS...", format: :dots)
           upload_spinner.auto_spin
           upload_bucket_name = ftl_service.get_default_bucket(gcp_project)
           app_gcs_link = upload_file(params[:app_path],
@@ -35,8 +47,7 @@ module Fastlane
                                      "#{gcs_workfolder}/#{DEFAULT_APP_BUNDLE_NAME}",
                                      gcp_project,
                                      gcp_credential)
-          # upload_spinner.stop("Upload completed!")
-          upload_spinner.success
+          upload_spinner.success("Done")
         end
 
         UI.message("Submitting job(s) to Firebase Test Lab")
@@ -66,13 +77,36 @@ module Fastlane
       end
 
       def self.wait_for_test_results(ftl_service, gcp_project, matrix_id)
-        spinner = TTY::Spinner.new("[:spinner] Waiting for test results...", format: :dots)
+        has_shown_console_link = false
+
+        spinner = TTY::Spinner.new("[:spinner] Starting tests...", format: :dots)
         spinner.auto_spin
+
         while true
           results = ftl_service.get_matrix_results(gcp_project, matrix_id)
-          case results["state"]
-          when "FINISHED"
-            spinner.stop("Done")
+          if !has_shown_console_link && !results["resultStorage"].nil? &&
+              !results["resultStorage"]["toolResultsExecution"].nil?
+            has_shown_console_link = true
+            tool_results_execution = results["resultStorage"]["toolResultsExecution"]
+            history_id = tool_results_execution["historyId"]
+            execution_id = tool_results_execution["executionId"]
+            spinner.success("Done")
+            UI.message("Click https://console.firebase.google.com" \
+              "/project/#{gcp_project}/testlab/histories/#{history_id}/matrices/#{execution_id} for more information " \
+              "about this run")
+            spinner = TTY::Spinner.new("[:spinner] Waiting for results...", format: :dots)
+            spinner.auto_spin
+          end
+
+          state = results["state"]
+          if ERROR_CODE_TO_MESSAGE.key?(state)
+            spinner.error("Failed")
+            UI.user_error!(ERROR_CODE_TO_MESSAGE[state])
+            return false
+          end
+
+          if state == "FINISHED"
+            spinner.success("Done")
             UI.message("Test jobs are completed")
             UI.message("-------------------------")
             UI.message("|        RESULTS        |")
@@ -91,34 +125,15 @@ module Fastlane
               UI.success("🎉All jobs finished successfully")
               return true
             end
-          when "ERROR"
-            UI.user_error!("The execution or matrix has stopped because it encountered an infrastructure failure.")
-            return false
-          when "UNSUPPORTED_ENVIRONMENT"
-            UI.user_error!("The execution was not run because it corresponds to a unsupported environment.")
-            return false
-          when "INCOMPATIBLE_ENVIRONMENT"
-            UI.user_error!("The execution was not run because the provided inputs are incompatible with the " \
-                           "requested environment")
-            return false
-          when "INCOMPATIBLE_ARCHITECTURE"
-            UI.user_error!("The execution was not run because the provided inputs are incompatible with the " \
-                           "requested architecture.")
-            return false
-          when "CANCELLED"
-            UI.user_error!("The user cancelled the execution.")
-            return false
-          when "INVALID"
-            UI.error!("The execution or matrix was not run because the provided inputs are not valid.")
-            return false
           end
+
           sleep(PULL_RESULT_INTERVAL)
         end
       end
 
       def self.generate_directory_name
-        ts_str = Time.now.getutc.strftime "%Y%m%d-%H%M%SZ"
-        return "fastlane-#{ts_str}-#{SecureRandom.hex[0..5]}"
+        timestamp = Time.now.getutc.strftime "%Y%m%d-%H%M%SZ"
+        return "fastlane-#{timestamp}-#{SecureRandom.hex[0..5]}"
       end
 
       #####################################################
