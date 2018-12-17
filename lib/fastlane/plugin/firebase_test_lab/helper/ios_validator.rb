16cafa342e695494837e141f6cdd54a242112706
John Rogers <jrogers@deputy.com>
2018-12-18 03:24:53 +1100

    Find and use the correct schema if the UI tests schema in the xctestrun file (#57)

--- lib/fastlane/plugin/firebase_test_lab/helper/ios_validator.rb
+++ lib/fastlane/plugin/firebase_test_lab/helper/ios_validator.rb
@@ -14,14 +14,29 @@ module Fastlane
             end
 
             conf = Plist.parse_xml(xctestrun_files.first.get_input_stream)
+
+            # Ensure we only have one scheme (other than the metadata if that's present)
             size = conf.size
             if conf['__xctestrun_metadata__']
               size -= 1
-            end  
-            unless size == 1
-              UI.user_error!("The app bundle may contain only one scheme, #{size} found")
             end
-            _, scheme_conf = conf.first
+            unless size == 1
+              UI.user_error!("The app bundle may contain only one scheme, #{size} found.")
+            end
+
+            # Find the tests scheme that's not the metadata scheme
+            scheme_conf = nil
+            conf.each do |key, value|
+              if scheme_conf.nil? && key != '__xctestrun_metadata__'
+                scheme_conf = value
+              end
+            end
+
+            # Ensure we found the tests scheme
+            if scheme_conf.nil?
+              UI.user_error!("Failed to find your UI tests scheme in your .xctestrun file.")
+            end
+
             unless scheme_conf["IsUITestBundle"]
               UI.user_error!("The app bundle is not a UI test bundle. Did you build with build-for-testing argument?")
             end
